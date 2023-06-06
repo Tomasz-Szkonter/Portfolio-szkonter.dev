70ec4a778aebb914780aa18376cb912cb60b54cd
Tomasz Szkonter <Tomasz.Szkonter@gmail.com>
2023-06-06 13:27:39 +0200

    Project restart

---
+++ Portfolio/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  mode: 'jit',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2ECC71',
+        secondary: '#54D98C',
+        'dark-100': '#263547',
+        'dark-200': '#384E68',
+        'black-100': '#020202',
+        'black-200': '#090325',
+        'gray-100': '#DFE0E0',
+        'gray-200': '#C5C7C7',
+        'white-100': '#ffffff',
+      },
+      boxShadow: {
+        card: '0px 35px 120px -15px #211e35',
+      },
+      screens: {
+        xs: '450px',
+      },
+      backgroundImage: {
+        'hero-pattern': "url('/src/assets/herobg.png')",
+      },
+    },
+  },
+  plugins: [],
+};
